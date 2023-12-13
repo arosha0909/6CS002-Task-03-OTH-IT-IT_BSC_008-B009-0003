692b1e5516d0a9f34c7e40eff468b423594185a7
Arosha Rukshan <rukshanarosha@gmail.com>
2023-12-13 23:49:40 +0530

    startergy with players

---
+++ src/Model/Aardvark.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Aardvark {
+
+	public String playerName;
+
+	public Aardvark(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void playGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/Model/SPlayertwo.java
+++ src/Model/SPlayertwo.java
@@ -1,3 +1,5 @@
+package Model;
+
 public class SPlayertwo implements Strategy{
 	
    @Override
